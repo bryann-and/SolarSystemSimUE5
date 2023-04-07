3da3749263e0d53065919143a987b336e5ae365c
Bryan Anderson Duarte <bryann.and@gmail.com>
2023-04-07 00:34:16 -0300

    BP creations

--- SolarSystemSim/Source/SolarSystemSim/Private/Planet.cpp
+++ SolarSystemSim/Source/SolarSystemSim/Private/Planet.cpp
@@ -13,15 +13,43 @@ APlanet::APlanet()
 
 // Called when the game starts or when spawned
 void APlanet::BeginPlay()
-{
-	Super::BeginPlay();
-	
+{	
+	CurrentVelocity = StatingVelocity;
+
+	Super::BeginPlay();	
 }
 
 // Called every frame
 void APlanet::Tick(float DeltaTime)
 {
+	/*TArray<APlanet*> array;
+	array.Init(this,1);*/
+
+	UpdateVelocity(PlanetsToConsider, DeltaTime);
 	Super::Tick(DeltaTime);
 
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Mass: %f"), MassKg));
+}
+
+void APlanet::UpdateVelocity(TArray<APlanet*> planetsToConsider, float deltaTime)
+{
+	
+	FVector currentLocation = this->GetActorLocation();
+
+	for (auto &planet : planetsToConsider)
+	{		
+		/*
+		* F= g*(m1*m2 / d^2)
+		*/
+		float distanceSqrt = (currentLocation - planet->GetActorLocation()).SquaredLength();
+		FVector direction = (currentLocation - planet->GetActorLocation());
+		direction.Normalize();
+		FVector force = direction * gravity * (this->MassKg * planet->MassKg / distanceSqrt);
+
+		FVector acceleration = force / this->MassKg;
+		CurrentVelocity += acceleration * deltaTime;
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Mass: %f"), planet->MassKg));
+	}
+	this->SetActorLocation(CurrentVelocity);
 }
 
--- SolarSystemSim/Source/SolarSystemSim/Public/Planet.h
+++ SolarSystemSim/Source/SolarSystemSim/Public/Planet.h
@@ -6,21 +6,32 @@
 #include "GameFramework/Actor.h"
 #include "Planet.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable)
 class SOLARSYSTEMSIM_API APlanet : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	APlanet();
+private:
+	FVector CurrentVelocity;	
+	double gravity = 0.00000066743;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:		
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector StatingVelocity;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float MassKg;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<APlanet*> PlanetsToConsider;
+
+	// Sets default values for this actor's properties
+	APlanet();
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	void UpdateVelocity(TArray<APlanet*> planetsToConsider, float deltaTime);
 
 };
